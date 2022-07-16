21ac54563789d0462714424678077294967c129c
lukaszmpiasecki <lukasz.piasecki@int.pl>
2022-07-16 13:05:34 +0200

    Start

---
+++ src/main/java/lpiasecki/app/LpiaseckiApplication.java
@@ -0,0 +1,13 @@
+package lpiasecki.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiaseckiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiaseckiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lpiasecki/app/LpiaseckiApplicationTests.java
@@ -0,0 +1,13 @@
+package lpiasecki.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpiaseckiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
